ebd3436d3e56f17261efdff34c507945ec5ca224
daiki510 <ddgg7755@gmail.com>
2022-12-11 22:44:34 +0900

    add scraping

--- src/sample.scraper.ts
+++ src/sample.scraper.ts
@@ -1,1 +1,12 @@
-console.log('hello');
+import puppeteer from 'puppeteer-core';
+
+(async () => {
+  const browser = await puppeteer.launch({
+    channel: 'chrome', //ここで指定することで既存のchromeを利用可能
+    headless: true, //falseの場合はブラウザ上での動きを確認しながら実行可能
+  });
+  const page = await browser.newPage();
+  await page.goto('https://www.google.co.jp/');
+  await page.screenshot({ path: `tmp/sample.png` });
+  await browser.close();
+})();
